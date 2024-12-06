f9ae16ce944ceafb120a88ae17442c1325275a6c
Eric Schwartz <eric.schwartz@calltrackingmetrics.com>
2024-12-06 09:53:55 -0500

    add test for outbound and inbound wrap up panel. prevent browser from closing on error to make the tests easier to debug

--- smoke-test.mjs
+++ smoke-test.mjs
@@ -1,3 +1,14 @@
+
+/*
+USAGE
+
+- Log into CTM in dev and prod
+- Make sure PHONE_TEST_DEV_TOKEN and PHONE_TEST_DEV_EMAIL are set with the current sessions' ctm_auth_cap tokens
+- Run this to start test:
+  node smoke-test.mjs <number-to-receive-calls> <number-to-send-calls-from>
+
+*/
+
 import puppeteer from "puppeteer";
 import process from 'process';
 
@@ -41,10 +52,19 @@ const browser = await puppeteer.launch(
   }
 );
 
+process.on('uncaughtException', async (err) => {
+  console.error('\n\x1b[31mUncaught Exception:\x1b[0m', err);
+  // if (process.env.DEBUG_PHONE_TEST) {
+    console.log('Keeping browser open for debugging...');
+    await new Promise(() => {}); // Prevent script from exiting
+  // }
+});
+
 const page = await browser.newPage();
+console.log(`loading page: http://app.ctmdev.us/phone_test?token=${token}&email=${email}`);
 await page.goto(`http://app.ctmdev.us/phone_test?token=${token}&email=${email}`);
 
-// //wait for the device to be registered, this can take a while, also make sure you accept the camera/mic permissions on chrome tab
+// wait for the device to be registered, this can take a while, also make sure you accept the camera/mic permissions on chrome tab
 // await deviceFrame.waitForSelector(".register-details", { visible: true, timeout: 45000 });
 
 const frameElement = await page.$('iframe[src*="https://app.ctmdev.us/phoneapp/embed"]');
@@ -61,10 +81,15 @@ await frame.evaluate(() => {
   document.querySelector('ctm-phone-control').agent.setStatus('online');
 });
 
-// Helper functions
+// Store settings in a var for later
+const phoneSettings = await frame.evaluate(() => {
+  return document.querySelector('ctm-phone-control').settings;
+});
+
+///// Helper functions ////
 
 const makeCallFromProd = async () => {
-  console.log('about to make call...');
+  console.log('about to make call from prod...');
   try {
     const url = `https://app.calltrackingmetrics.com/api/v1/accounts/${process.env.PHONE_TEST_PROD_ACCOUNT_ID}/calls/`;
 
@@ -93,7 +118,43 @@ const makeCallFromProd = async () => {
   };
 }
 
-// Test functions
+const getWrapUpPanelSelector = (key) => {
+  switch (key) {
+    case 'caller_profile':
+      return 'ctm-contact-panel';
+    case 'caller_sale':
+      return 'ctm-score-panel';
+    default:
+      return 'ctm-custom-panel';
+  }
+};
+
+const sleep = (ms) => {
+  console.log('sleeping for', ms);
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const makeInboundCall = async () => {
+  await makeCallFromProd();
+
+  // Answer the call!
+  await frame.locator('.accept-call-button', { timeout: 8000 }).click();
+
+  // Wait until we are showing "inbound" status which means the call has connected
+  await frame.locator('.agent-status-inbound').click();
+
+  const status = await frame.locator('main').map((main) => main.dataset.status).wait();
+  // console.log('status', status);
+
+  const callDetails = await frame.locator('ctm-phone-control').map((control) => control.callDetails).wait();
+  // console.log('callDetails', callDetails);
+
+  console.log('waiting 8 secs before hanging up...');
+  await sleep(6000);
+  frame.click("a.hangup-call-button");
+}
+
+///// Tests /////
 
 const testOutboundCall = async () => {
   await frame.evaluate((prodNumber) => {
@@ -103,39 +164,48 @@ const testOutboundCall = async () => {
 
   const callButton = await frame.waitForSelector('.call-button', { visible: true, timeout: 2000 });
   await callButton.click();
+  console.log(`Making outbound call to ${prodNumber}...`);
 
-  // await frame.locator('a.hangup-call-button', { hidden: true, timeout: 1000 }).click(); // If the call actually connects, we need to hang up
+  // await frame.locator('a.hangup-call-button', { hidden: true, timeout: 1000 }).click(); This will test the fast hangup issue! Uncomment when we fix that.
 
+  await sleep(6000);
+  frame.click("a.hangup-call-button");
+
+  const wrapUpSelector = getWrapUpPanelSelector(phoneSettings.wrap_up_panel)
+  console.log('waiting for wrap up panel:', wrapUpSelector);
+  await frame.waitForSelector(wrapUpSelector, { visible: true, timeout: 2000 });
+
+  // Set status to Ready in case it isn't already
+  // await frame.evaluate(() => {
+  //   document.querySelector('ctm-phone-control').agent.setStatus('online');
+  // });
+  await frame.waitForSelector('.finish-wrapup', { visible: true, timeout: 2000 });
   await frame.locator('.finish-wrapup').click();
+
+  console.log('\n\x1b[32m%s\x1b[0m', 'testOutboundCall passed\n');
 }
 
 const testInboundCall = async () => {
-  await makeCallFromProd();
+  await makeInboundCall();
 
-  // Answer the call!
-  await frame.locator('.accept-call-button').click();
-
-  // Wait until we are showing "inbound" status which means the call has connected
-  await frame.locator('.agent-status-inbound').click();
-
-  const status = await frame.locator('main').map((main) => main.dataset.status).wait();
-  console.log('status', status);
-
-  const callDetails = await frame.locator('ctm-phone-control').map((control) => control.callDetails).wait();
-  console.log('callDetails', callDetails);
-
-  console.log('waiting 8 secs before hanging up...');
-  await setTimeout(() => {
-    frame.click("a.hangup-call-button");
-  }, 8000);
+  const inboundWrapUpSelector = getWrapUpPanelSelector(phoneSettings.inbound_wrap_up_panel);
+  console.log('waiting for inbound wrap up panel:', inboundWrapUpSelector);
+  await sleep(2000); // Wrapup panel might get "loaded" multiple times. Make sure it's the last time
+  await frame.waitForSelector(inboundWrapUpSelector, { visible: true, timeout: 2000 });
+  await frame.waitForSelector('.finish-wrapup', { visible: true, timeout: 2000 });
 
   await frame.locator('.finish-wrapup').click();
+
+  console.log('\n\x1b[32m%s\x1b[0m', 'testInboundCall passed\n');
 }
+
+///// Runner /////
 
 const runTests = async () => {
   await testOutboundCall();
   await testInboundCall();
 
+  console.log('\n\x1b[32m%s\x1b[0m', 'All tests passed!\n');
   process.exit(0);
 }
 
