7998d36164b33a5c2615677aa28b6ac07b2a00d9
Robert Stuckey <robert.stuckey@calltrackingmetrics.com>
2024-12-13 16:36:44 -0500

    Make sure you can access all the basic panels

--- smoke-test.mjs
+++ smoke-test.mjs
@@ -154,6 +154,26 @@ const makeInboundCall = async () => {
   frame.click("a.hangup-call-button");
 }
 
+const logSuccess = (msg = null) => {
+  if (!msg) {
+    // Capture the stack traceQ
+    const stack = {};
+    Error.captureStackTrace(stack);
+
+    // The stack trace is stored as a string in stack.stack
+    const stackLines = stack.stack.split('\n');
+    const callerLine = stackLines[2]; // The third line is typically where the caller function is
+
+    // Extract the function name using a regular expression
+    const functionNameMatch = callerLine.match(/at (\S+)/);
+    const functionName = functionNameMatch ? functionNameMatch[1] : 'UnknownFunction';
+
+    msg = `${functionName} passed`;
+  }
+
+  console.log('\n\x1b[32m%s\x1b[0m', `${msg}`);
+};
+
 ///// Tests /////
 
 const testOutboundCall = async () => {
@@ -182,7 +202,7 @@ const testOutboundCall = async () => {
   await frame.waitForSelector('.finish-wrapup', { visible: true, timeout: 2000 });
   await frame.locator('.finish-wrapup').click();
 
-  console.log('\n\x1b[32m%s\x1b[0m', 'testOutboundCall passed\n');
+  logSuccess();
 }
 
 const testInboundCall = async () => {
@@ -196,7 +216,188 @@ const testInboundCall = async () => {
 
   await frame.locator('.finish-wrapup').click();
 
-  console.log('\n\x1b[32m%s\x1b[0m', 'testInboundCall passed\n');
+  logSuccess();
+}
+
+const testParkedPanel = async () => {
+  await testParkedPanelAccessible();
+  await testParkedPanelCloseable();
+}
+
+const testParkedPanelAccessible = async () => {
+  await frame.locator('.parked-calls').click();
+  await frame.waitForSelector('.parked-options .list-of-calls', { visible: true, timeout: 2000 });
+
+  logSuccess();
+}
+
+const testParkedPanelCloseable = async () => {
+  await frame.locator('.close-parked-view').click();
+  await frame.waitForSelector('.parked-options .list-of-calls', { hidden: true, timeout: 2000 });
+
+  logSuccess();
+}
+
+const testKeypadPanel = async () => {
+  await testKeypadPanelAccessible();
+  await testKeypadNumKeys();
+  await testChangeStatusPickerToFromNumerOnFullInput();
+  await showCallButtonOnFullInput();
+  await testKeypadPanelCloseable();
+}
+
+const testKeypadPanelAccessible = async () => {
+  await frame.locator('.button-keypad').click();
+  await frame.waitForSelector('.keypad', { visible: true, timeout: 2000 });
+
+  logSuccess();
+}
+
+const testKeypadNumKeys = async () => {
+  // Loop through all 10 numbers and click them all and make sure the number is added to the text input
+  for (let i = 0; i < 10; i++) {
+    await frame.locator(`#button_${i}`).click();
+    const phoneInputValue = await frame.evaluate(() => {
+      const inputElement = document.querySelector('.phonenumber-input');
+      return inputElement ? inputElement.value.slice(-1) : '';
+    });
+
+    if (phoneInputValue !== i.toString()) {
+      console.error(`Keypad key ${i} failed!`);
+      process.exit(1);
+    }
+  }
+  
+  logSuccess();
+}
+
+const testChangeStatusPickerToFromNumerOnFullInput = async () => {
+  // Clear the input field
+  await frame.evaluate(() => {
+    const inputElement = document.querySelector('.phonenumber-input');
+    if (inputElement) {
+      inputElement.value = ''; 
+    }
+  });
+  // Cleaing the input causes us to close the keypad, so reopen it
+  await frame.locator('.button-keypad').click();
+
+  await frame.waitForSelector('.agent-status-picker', { visible: true, timeout: 2000 });
+
+  // Fill the input with 10 numbers
+  for (let i = 0; i < 10; i++) {
+    await frame.locator(`#button_${i}`).click();
+  }
+
+  await frame.waitForSelector('.from_number', { visible: true, timeout: 2000 });
+
+  logSuccess();
+}
+
+const showCallButtonOnFullInput = async () => {
+  // Clear the input field
+  await frame.evaluate(() => {
+    const inputElement = document.querySelector('.phonenumber-input');
+    if (inputElement) {
+      inputElement.value = ''; 
+    }
+  });
+  // Cleaing the input causes us to close the keypad, so reopen it
+  await frame.locator('.button-keypad').click();
+
+  await frame.waitForSelector('.call-button', { visible: false, timeout: 2000 });
+
+  // Fill the input with 10 numbers
+  for (let i = 0; i < 10; i++) {
+    await frame.locator(`#button_${i}`).click();
+  }
+
+  await frame.waitForSelector('.call-button', { visible: true, timeout: 2000 });
+
+  logSuccess();
+}
+
+const testKeypadPanelCloseable = async () => {
+  await frame.locator('.toggle-keypad').click();
+  await frame.waitForSelector('.keypad', { hidden: true, timeout: 2000 });
+
+  logSuccess();
+}
+
+const testQueuePanel = async () => {
+  await testQueuePanelAccessible();
+  await testQueuePanelCloseable();
+}
+
+const testQueuePanelAccessible = async () => {
+  await frame.locator('button.queue').click();
+  await frame.waitForSelector('.queue-list', { visible: true, timeout: 2000 });
+
+  logSuccess();
+}
+
+const testQueuePanelCloseable = async () => {
+  await frame.locator('.close-queue-view').click();
+  await frame.waitForSelector('.queue-list', { hidden: true, timeout: 2000 });
+
+  logSuccess();
+}
+
+const testHistoryTab = async () => {
+  await testHistoryTabAccessible();
+  await testHistoryTabCloseable();
+}
+
+const testHistoryTabAccessible = async () => {
+  await frame.locator('#history').click();
+  await frame.waitForSelector('activity-history', { visible: true, timeout: 2000 });
+
+  logSuccess();
+}
+
+const testHistoryTabCloseable = async () => {
+  await frame.locator('.close-history-view').click();
+  await frame.waitForSelector('activity-history', { hidden: true, timeout: 2000 });
+
+  logSuccess();
+}
+
+const testChatTab = async () => {
+  await testChatTabAccessible();
+  await testChatTabCloseable();
+}
+
+const testChatTabAccessible = async () => {
+  await frame.locator('#chat').click();
+  await frame.waitForSelector('chat-history', { visible: true, timeout: 2000 });
+
+  logSuccess();
+}
+
+const testChatTabCloseable = async () => {
+  await frame.locator('.close-chat-view').click();
+  await frame.waitForSelector('chat-history', { hidden: true, timeout: 2000 });
+
+  logSuccess();
+}
+
+const testTextTab = async () => {
+  await testTextTabAccessible();
+  await testTextTabCloseable();
+}
+
+const testTextTabAccessible = async () => {
+  await frame.locator('#text').click();
+  await frame.waitForSelector('text-message-list', { visible: true, timeout: 2000 });
+
+  logSuccess();
+}
+
+const testTextTabCloseable = async () => {
+  await frame.locator('.close-text-view').click();
+  await frame.waitForSelector('text-message-list', { hidden: true, timeout: 2000 });
+
+  logSuccess();
 }
 
 ///// Runner /////
@@ -204,8 +405,14 @@ const testInboundCall = async () => {
 const runTests = async () => {
   await testOutboundCall();
   await testInboundCall();
+  await testParkedPanel();
+  await testKeypadPanel();
+  await testQueuePanel();
+  await testHistoryTab();
+  await testChatTab();
+  await testTextTab();
 
-  console.log('\n\x1b[32m%s\x1b[0m', 'All tests passed!\n');
+  logSuccess('All tests passed!');
   process.exit(0);
 }
 
